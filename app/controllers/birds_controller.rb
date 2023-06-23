15b48fb3f0c0d9441f3148d4c692a70a765a4d65
brigitteorina <brigitteorina@gmail.com>
2023-06-23 14:36:28 +0300

    complete

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -45,9 +45,20 @@ class BirdsController < ApplicationController
   end
 
   private
+  
 
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+#DESTROY
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
   end
 
 end
